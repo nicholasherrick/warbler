a202acc9174b85e9bb68fecf5e6adf477ada5149
Nicholas Herrick <nth.herrick@gmail.com>
2020-06-16 14:39:15 -0600

    added styling to navbar, added images, fixed navbar bootstrap divs

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Logo from '../images/warbler-logo.png';
 
 class Navbar extends Component {
   render() {
     return (
       <nav className='navbar navbar-expand'>
         <div className='container-fluid'>
-          <Link to='/' className='navbar-brand'>
-            <img src='' alt='Warbler Home' />
-          </Link>
+          <div className='navbar-header'>
+            <Link to='/' className='navbar-brand'>
+              <img src={Logo} alt='Warbler Home' />
+            </Link>
+          </div>
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <Link to='/register'>Register</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+          </ul>
         </div>
-        <ul className='nav navbar-nav navbar-right'>
-          <li>
-            <Link to='/register'>Register</Link>
-          </li>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-        </ul>
       </nav>
     );
   }
