9acd120fd6506db3a053620d3b07661cd2350a35
Nicholas Herrick <nth.herrick@gmail.com>
2020-06-15 22:55:46 -0600

    created user model for mongoose and hashed user passwords with bcrypt

---
+++ server/models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Define user Schema
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profileImageUrl: {
+    type: String,
+  },
+});
+
+// Bcrypt hook & hash
+userSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Method checks if password is correct
+userSchema.method.comparePassword = async function (candidatePassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Create model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
